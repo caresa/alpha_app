83b414ba20acbd8a4c420298b422d1b3e6f14434
Caresa Huddleston <caresa.huddleston@gmail.com>
2014-07-28 21:00:33 -0500

    a shit ton of json

--- public/js/slides.js
+++ public/js/slides.js
@@ -7,7 +7,8 @@ var slides =[
       {
         'src': './public/images/a/anteater.jpg',
         'classes': 'pic-border1',
-        'id': ''
+        'id': '',
+        'english': 'anteater'
 
       }
     ]
@@ -20,19 +21,22 @@ var slides =[
       {
         'src': './public/images/b/ball.jpg',
         'classes': 'polaroid',
-        'id': ''
+        'id': '',
+        'english': 'ball'
 
       },
       {
         'src': './public/images/b/bike.jpg',
         'classes': 'polaroid',
-        'id': '1'
+        'id': '',
+        'english': 'bike'
 
       },
       {
         'src': './public/images/b/book.jpg',
         'classes': 'polaroid',
-        'id': '2'
+        'id': '',
+        'english': 'book'
       },
 
     ]
@@ -45,19 +49,22 @@ var slides =[
       {
         'src': './public/images/c/car.jpg',
         'classes': 'pic-border',
-        'id': ''
+        'id': '',
+        'english': 'car'
 
       },
       {
         'src': './public/images/c/cascarone.jpg',
         'classes': 'pic-border',
-        'id': '1'
+        'id': '',
+        'english': 'egg'
 
       },
       {
         'src': './public/images/c_is_for_capitol.jpg',
         'classes': 'pic-border',
-        'id': '2'
+        'id': '',
+        'english': 'capitol'
       },
     ]
   },
@@ -69,19 +76,22 @@ var slides =[
       {
         'src': './public/images/d/dance.jpg',
         'classes': 'polaroid',
-        'id': ''
+        'id': '',
+        'english': 'dance'
 
       },
       {
         'src': './public/images/d/dog.jpg',
         'classes': 'polaroid',
-        'id': '1'
+        'id': '',
+        'english': 'dog'
 
       },
       {
         'src': './public/images/d/deer.jpg',
         'classes': 'polaroid',
-        'id': '2'
+        'id': '',
+        'english': 'deer'
       },
     ]
   },
@@ -91,22 +101,568 @@ var slides =[
     'letterImg': 'letter_e_clipped_rev_1.png',
     'images': [
       {
-        'src': './public/images/d/dance.jpg',
+        'src': './public/images/e/eat.jpg',
         'classes': 'polaroid',
-        'id': ''
+        'id': '',
+        'english': 'eat'
 
       },
       {
-        'src': './public/images/d/dog.jpg',
+        'src': './public/images/e/eight.jpg',
         'classes': 'polaroid',
-        'id': '1'
+        'id': '',
+        'english': 'eight'
 
       },
       {
-        'src': './public/images/d/deer.jpg',
+        'src': './public/images/e/eel.jpg',
         'classes': 'polaroid',
-        'id': '2'
+        'id': '',
+        'english': 'eel'
       },
     ]
   },
+  {
+    'letter': 'f',
+    'title': 'iesta Gardens',
+    'letterImg': 'letter_e_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/f/feather_clipped_rev_1.png',
+        'classes': 'slide',
+        'id': '',
+        'english': 'feather'
+
+      },
+      {
+        'src': './public/images/f/fox.jpg',
+        'classes': 'pic-border1',
+        'id': '',
+        'english': 'fox'
+
+      },
+      {
+        'src': './public/images/f/five.jpg',
+        'classes': 'pic-border1',
+        'id': '',
+        'english': 'five'
+      },
+      {
+        'src': './public/images/f/fiesta_gardens.jpg',
+        'classes': 'pic-border1',
+        'id': '',
+        'english': 'gardens'
+      },
+    ]
+  },
+  {
+    'letter': 'g',
+    'title': 'raffiti Park',
+    'letterImg': 'letter_g_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/g/graffit_2.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'graffiti'
+
+      },
+      {
+        'src': './public/images/g/give.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'give'
+
+      },
+      {
+        'src': './public/images/g/grouch.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'grouch'
+      }
+    ]
+  },
+  {
+    'letter': 'h',
+    'title': 'ouse Park',
+    'letterImg': 'letter_h_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/h/home.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'home'
+
+      },
+      {
+        'src': './public/images/h/happy.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'happy'
+
+      },
+      {
+        'src': './public/images/h/heart.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'heart'
+      }
+    ]
+  },
+  {
+    'letter': 'i',
+    'title': 'maginarium',
+    'letterImg': 'letter_i_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/i/igloo.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'igloo'
+
+      },
+      {
+        'src': './public/images/i/imaginarium.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'museum'
+
+      },
+      {
+        'src': './public/images/i/island.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'island'
+      }
+    ]
+  },
+  {
+    'letter': 'j',
+    'title': 'uiceland',
+    'letterImg': 'letter_j_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/j/jump.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'jump'
+
+      },
+      {
+        'src': './public/images/j/jazz.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'jazz'
+
+      },
+      {
+        'src': './public/images/j/juggle.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'juggle'
+      }
+    ]
+  },
+  {
+    'letter': 'l',
+    'title': 'adybird Lake',
+    'letterImg': 'letter_l_clipped_rev_2.png',
+    'images': [
+      {
+        'src': './public/images/l/love.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'love'
+
+      },
+      {
+        'src': './public/images/l/laugh.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'laugh'
+
+      },
+      {
+        'src': './public/images/l/ladybird.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'ladybird'
+      }
+    ]
+  },
+  {
+    'letter': 'm',
+    'title': 'ount Bonnell',
+    'letterImg': 'letter_m_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/m/math.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'math'
+
+      },
+      {
+        'src': './public/images/m/mom.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'mother'
+
+      },
+      {
+        'src': './public/images/m/moon.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'moon'
+      }
+    ]
+  },
+  {
+    'letter': 'n',
+    'title': 'ature Center',
+    'letterImg': 'letter_n_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/n/night.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'night'
+
+      },
+      {
+        'src': './public/images/n/nine.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'nine'
+
+      },
+      {
+        'src': './public/images/n/nectarine_tree.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'nectarine tree'
+      }
+    ]
+  },
+  {
+    'letter': 'o',
+    'title': 'bservatory',
+    'letterImg': 'letter_o_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/o/octopus.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'octopus'
+
+      },
+      {
+        'src': './public/images/o/oval.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'ogre'
+
+      },
+      {
+        'src': './public/images/o/ogre.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'ogre'
+      }
+    ]
+  },
+  {
+    'letter': 'p',
+    'title': 'ine Cone',
+    'letterImg': 'letter_p.jpg',
+    'images': [
+      {
+        'src': './public/images/p/pine_cone.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'pine cone'
+
+      },
+      {
+        'src': './public/images/p/pen.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'pen'
+
+      },
+      {
+        'src': './public/images/p/play.jpg',
+        'classes': 'polaroid',
+        'id': '2'
+      }
+    ]
+  },
+  {
+    'letter': 'q',
+    'title': 'uacks',
+    'letterImg': 'letter_q_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/q/quail.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'quail'
+
+      },
+      {
+        'src': './public/images/q/quilt.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'quilt'
+
+      },
+      {
+        'src': './public/images/q/quote.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'quote'
+      }
+    ]
+  },
+  {
+    'letter': 'r',
+    'title': 'ed Bud',
+    'letterImg': 'letter_r_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/r/rocker.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'quail'
+      },
+      {
+        'src': './public/images/r/run.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'run'
+
+      },
+      {
+        'src': './public/images/r/rainbow.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'rainbow'
+
+      }
+    ]
+  },
+  {
+    'letter': 's',
+    'title': 'ymphony',
+    'letterImg': 'letter_s_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/s/sailboat.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'sailboat'
+      },
+      {
+        'src': './public/images/s/sunshine.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'sunshine'
+
+      },
+      {
+        'src': './public/images/q/quote.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'solar-system'
+      }
+    ]
+  },
+  {
+    'letter': 't',
+    'title': 'ower',
+    'letterImg': 'letter_t_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/t/toes.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'toes'
+      },
+      {
+        'src': './public/images/t/tail.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'tail'
+
+      },
+      {
+        'src': './public/images/t/talk.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'talk'
+      }
+    ]
+  },
+  {
+    'letter': 'u',
+    'title': 'niversity',
+    'letterImg': 'letter_u_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/u/uke.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'ukulele'
+      },
+      {
+        'src': './public/images/u/umbrella.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'umbrella'
+
+      },
+      {
+        'src': './public/images/u/unicorn.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'unicorn'
+      }
+    ]
+  },
+  {
+    'letter': 'v',
+    'title': 'elloway',
+    'letterImg': 'letter_v_clipped_rev_1.png',
+    'images': [
+      {
+        'src': './public/images/v/violet.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'ukulele'
+      },
+      {
+        'src': './public/images/u/umbrella.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'umbrella'
+
+      },
+      {
+        'src': './public/images/v/velociraptor.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'velociraptor'
+      }
+    ]
+  },
+  {
+    'letter': 'w',
+    'title': 'aller Creek',
+    'letterImg': 'letter_w.jpg',
+    'images': [
+      {
+        'src': './public/images/w/weird.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'weird'
+      },
+      {
+        'src': './public/images/w/wasp.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'wasp'
+
+      },
+      {
+        'src': './public/images/w/walrus.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'walrus'
+      }
+    ]
+  },
+  {
+    'letter': 'x',
+    'title': 'Games',
+    'letterImg': 'letter_x.jpg',
+    'images': [
+      {
+        'src': './public/images/x/xylophone.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'xylophone'
+      },
+      {
+        'src': './public/images/x/xray.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'xray'
+
+      },
+      {
+        'src': './public/images/x/xiphias.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'xiphias'
+      }
+    ]
+  },
+  {
+    'letter': 'y',
+    'title': 'mca',
+    'letterImg': 'letter_y.jpg',
+    'images': [
+      {
+        'src': './public/images/y/yak.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'yak'
+      },
+      {
+        'src': './public/images/y/yams.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'yams'
+
+      },
+      {
+        'src': './public/images/y/yawn.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'yawn'
+      }
+    ]
+  },
+  {
+    'letter': 'z',
+    'title': 'ilker',
+    'letterImg': 'letter_z_clipped_rev.jpg',
+    'images': [
+      {
+        'src': './public/images/z/zilker_2.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'park'
+      },
+      {
+        'src': './public/images/z/zebra.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'zebra'
+
+      },
+      {
+        'src': './public/images/z/zeppelin.jpg',
+        'classes': 'polaroid',
+        'id': '',
+        'english': 'zeppelin'
+      }
+    ]
+  },
+
+
+
+
+
+
 ];
